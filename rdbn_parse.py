47a8da6ffe77c8249ca120588c1aa95e8d6ffbc1
nyantoge <nyantoge@gmail.com>
2020-07-25 23:57:17 +0900

    fix incorrect table ordering

--- rdbn_parse.py
+++ rdbn_parse.py
@@ -38,13 +38,13 @@ def parse(f, dbfile):
     logger.debug("unk2 = 0x{:04x}".format(unk2))
     logger.debug("data_size = 0x{:08x}".format(data_size))
 
-    table_count, unk22, unk23, unk24, unk25, unk31, unk32, unk33, unk34, string_offset = \
+    table_count, unk22, type_count, unk24, list_count, unk31, unk32, unk33, unk34, string_offset = \
         struct.unpack("<16x6x5H5H6x", f.read(48))
     logger.debug("table_count = {}".format(table_count))
     logger.debug("unk22 = 0x{:04x}".format(unk22))
-    logger.debug("unk23 = 0x{:04x}".format(unk23))
+    logger.debug("type_count = 0x{:04x}".format(type_count))
     logger.debug("unk24 = 0x{:04x}".format(unk24))
-    logger.debug("unk25 = 0x{:04x}".format(unk25))
+    logger.debug("list_count = 0x{:04x}".format(list_count))
     logger.debug("unk31 = 0x{:04x}".format(unk31))
     logger.debug("unk32 = 0x{:04x}".format(unk32))
     logger.debug("unk33 = 0x{:04x}".format(unk33))
@@ -58,49 +58,43 @@ def parse(f, dbfile):
 
     f.seek(data_offset)
 
-    tables = []
+    tmp_tables = []
     child_counts = []
-
-    while True:
+    for i in range(table_count + type_count):
         name_crc, unk1, unk2, unk3, child_count, unk4 = struct.unpack("<I 2H 2H I", f.read(16))
         logger.debug(
             "col {:<30}: {:4x}, {:4x}, size: {}, has {} children, offset: {}."
             .format(strings_table[name_crc], unk1, unk2, unk3, child_count, unk4))
-
+        flag = f.read(1)[0] # 1 if it has no child?
+        if flag != (0 if child_count else 1):
+            logger.warning("unexpected use of flag: (flag={}, children={})".format(flag, child_count))
         col = {"name": strings_table[name_crc],
                "unk1": unk1,
                "unk2": unk2,
                "size": unk3,
                "offset": unk4,
-               "children": []
+               "children": [],
+               "flag": flag,
         }
-        flag = f.read(1)[0] # 1 if it has no child?
-        if flag != (0 if child_count else 1):
-            logger.warning("unexpected use of flag: (flag={}, children={})".format(flag, child_count))
-
-        if child_count:
-            tables.append(col)
-            child_counts.append(child_count)
-        else:
-            # child node
-            v = next((c for c in child_counts if c > 0), None)
-            if not v:
-                logger.error("no parent found")
-                return False
-            tables[child_counts.index(v)]["children"].append(col)
-            child_counts[child_counts.index(v)] = v - 1
         f.read(15)
-        if sum(child_counts) == 0:
-            break
 
-    if table_count != len(tables):
-        logger.warning("incorrect table count")
+        if child_count == 0:
+            val = next((x for x in child_counts if x > 0))
+            idx = child_counts.index(val)
+            child_counts[idx] = val - 1
+            tmp_tables[idx]["children"].append(col)
+        else:
+            child_counts.append(child_count)
+            tmp_tables.append(col)
+    tables = {x["name"]: x for x in tmp_tables}
 
     lists = {}
-    for i in range(table_count):
+    for i in range(list_count):
         idx, unk, offset, size, count = struct.unpack("<2HIII", f.read(16))
         listname_crc = int.from_bytes(f.read(4), "little")
         lists[strings_table[listname_crc]] = {
+            "index": idx,
+            "name": strings_table[listname_crc],
             "unk": unk,
             "offset": offset,
             "size": size,
@@ -109,44 +103,49 @@ def parse(f, dbfile):
         if not ensure_allzero(f.read(12)):
             logger.warning("nonzero value found after list name id")
 
-    tmp = []
-    keylist = []
-    for i in range(table_count * 2 + sum(len(x["children"]) for x in tables)):
-        tmp.append(strings_table[int.from_bytes(f.read(4), "little")])
-    for i in tmp:
-        keylist.append((i, int.from_bytes(f.read(4), "little")))
-    logger.debug("end: 0x{:04x}".format(f.tell()))
-
     con = sqlite3.connect(dbfile)
 
-    keylist_table = keylist[:table_count]
-    keylist = keylist[table_count:]
-    while keylist:
-        # fetch list name
-        lname = keylist_table.pop(0)
+    # list-table relationship and unknown property for each types
+    list_order = []
+    list_table = {}
+    for _ in range(list_count):
+        list_order.append(strings_table[int.from_bytes(f.read(4), "little")])
+    idx = 0
+    for _ in range(type_count + table_count):
+        name = strings_table[int.from_bytes(f.read(4), "little")]
+        if name in tables:
+            list_name = [x for x in lists.values() if x["index"] == idx][0]["name"]
+            list_table[list_name] = name
+            idx += 1
+    for _ in range(list_count + type_count + table_count):
+        # skip unknown values
+        f.read(4)
 
-        # table name
-        tname = keylist.pop(0)
+    for list_name in list_order:
+        table_name = list_table[list_name]
+
+        # fetch list
+        list_struct = lists[list_name]
+
+        # fetch type name
+        table_type = tables[table_name]
+
+        logger.debug("list {} starts at 0x{:08x}".format(list_name, f.tell()))
 
         # get entry size and count from lists information
-        size = lists[lname[0]]["size"]
-        count = lists[lname[0]]["count"]
+        size = list_struct["size"]
+        count = list_struct["count"]
 
         # get table information
-        structure = [x for x in tables if x["name"] == tname[0]][0]
-        columns = ", ".join("{} {}".format(x["name"], get_type(x["unk1"])[0]) for x in structure["children"])
-        con.execute("CREATE TABLE IF NOT EXISTS {} ({});".format(structure["name"], columns))
+        columns = ", ".join("{} {}".format(c["name"], get_type(c["unk1"])[0]) for c in table_type["children"])
 
-        # TODO: skip entries
-        # these data should contain metadata on columns
-        for i in structure["children"]:
-            keylist.pop(0)
+        con.execute("CREATE TABLE IF NOT EXISTS {} ({});".format(table_name, columns))
 
         # insert information
         for i in range(count):
             row_data = f.read(size)
             row_out = []
-            for child in structure["children"]:
+            for child in table_type["children"]:
                 type_ = get_type(child["unk1"])
                 if type_ is None:
                     logger.error("unknown data type {} (size: {})".format(child["unk1"], child["size"]))
@@ -161,7 +160,7 @@ def parse(f, dbfile):
                 else:
                     row_out.append(data)
             placeholder = ", ".join("?" * len(row_out))
-            con.execute("INSERT INTO {} VALUES ({});".format(structure["name"], placeholder), row_out)
+            con.execute("INSERT INTO {} VALUES ({});".format(table_name, placeholder), row_out)
     con.commit()
     con.close()
     logger.debug("proccessing finished at 0x{:08x}".format(f.tell()))
